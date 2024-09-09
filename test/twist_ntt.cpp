5bf57b1875b709f6641b18304373cf64ac8ceee6
Navjeet Sandhu <navjeet.sandhu@gmail.com>
2024-09-09 15:42:42 -0400

    test bit shift only

--- test/twist_ntt.cpp
+++ test/twist_ntt.cpp
@@ -54,6 +54,69 @@ int  readFromFile(std::vector<uint32_t>& data, bool flag = false) {
 }
 
 
+void  applyBitShifting(std::vector<uint32_t>& a, int count) {
+    constexpr uint64_t lvl1P = 1073707009;
+    //constexpr uint64_t lvl1P = 1073741783;
+    //constexpr uint64_t lvl1P = 1073741789;
+    static constexpr std::uint32_t nbit = 10;
+    static constexpr std::uint32_t n = 1 << nbit;  // dimension
+    std::cout << "Bit shift testing " << count << " Input: " << std::endl;
+    std::cout << std::dec;
+    int i;
+    for (i = 0; i < count; i++) {
+        std::cout << a[i] << " ";
+    }
+    std::cout << std::endl;
+    std::cout << std::hex;
+    std::cout  << "Input in HEX: " << std::endl;
+    for (i = 0; i < count; i++) {
+        std::cout << std::hex << a[i] << " ";
+    }
+    std::cout << std::endl << std::endl;
+
+    std::array<uint64_t, n> temp{};
+    for (i = 0; i <n; i++)
+            temp[i] = (lvl1P * static_cast<uint64_t>(a[i])) >> 32;
+
+
+    std::cout << "After (lvl1P * static_cast<uint64_t>(a[i])) >> 32:" << std::endl;
+    std::cout << std::dec;
+    for (i = 0; i < count; i++) {
+        std::cout << temp[i] << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "After (lvl1P * static_cast<uint64_t>(a[i])) >> 32: in HEX:" << std::endl;
+    std::cout << std::hex;
+    for (i = 0; i < count; i++) {
+        std::cout << temp[i] << " ";
+    }
+    std::cout << std::endl;
+
+    std::array<uint64_t, n> res{};
+
+    std::cout << std::endl;
+
+    for (i = 0; i < n; i++) res[i] = (temp[i] << 32) / lvl1P;
+
+    std::cout << "Result after (temp[i] << 32) / lvl1P: " << std::endl;
+    std::cout << std::dec;
+    for (i = 0; i < count; i++) {
+        std::cout << res[i] << " ";
+    }
+    std::cout << std::endl;
+
+
+    std::cout << "Result after (temp[i] << 32) / lvl1P in HEX:" << std::endl;
+    std::cout << std::hex;
+    for (i = 0; i < count; i++) {
+        std::cout << res[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+
+
 int main(int argc, char *argv[]) {
     uint32_t num_print = TFHEpp::lvl1param::n;
     int test_case = -1;
@@ -76,14 +139,6 @@ int main(int argc, char *argv[]) {
                 test_case = std::stoi(argv[i]);
                 printf("test_case: %d ", test_case );
 
-                if(test_case == 3) {
-                    storeInFile();
-                    return 1;
-                }
-                if(test_case == 4) {
-                    readFromFile(data, true);
-                    return 1;
-                }
 
             }
         }
@@ -101,20 +156,30 @@ int main(int argc, char *argv[]) {
     }
     std::array<cuHEpp::INTorus, TFHEpp::lvl1param::n> resntt, resntt1, resntt2;
 
-    if( test_case == 0 ||  test_case < 0 ||  test_case > 3) {
+    if( test_case == 0) {
         std::cout << std::endl << "\nStart HEXL test" << std::endl;
         TFHEpp::TwistINTT_lvl1param_test<TFHEpp::lvl1param>(resntt, a, 0, num_print);
         TFHEpp::TwistNTT_lvl1param_test<TFHEpp::lvl1param>(res0, resntt, 0, num_print);
     }
-    if (test_case == 1 || test_case < 0 || test_case > 3) {
+    if (test_case == 1) {
         std::cout << std::endl << "\nStart HEXL test without bit shifting" << std::endl;
         TFHEpp::TwistINTT_lvl1param_test<TFHEpp::lvl1param>(resntt, a, 1, num_print);
         TFHEpp::TwistNTT_lvl1param_test<TFHEpp::lvl1param>(res0, resntt, 1, num_print);
     }
-    if (test_case == 2 || test_case < 0 || test_case > 3) {
+    if (test_case == 2) {
         std::cout << std::endl << "\nStart test without HEXL" << std::endl;
         TFHEpp::TwistINTT_lvl1param_test<TFHEpp::lvl1param>(resntt, a, 2, num_print);
         TFHEpp::TwistNTT_lvl1param_test<TFHEpp::lvl1param>(res0, resntt, 2, num_print);
+    }
+    if(test_case == 3) {
+        storeInFile();
+    }
+    if(test_case == 4) {
+        readFromFile(data, true);
+    }
+
+    if (test_case == 5) {
+        applyBitShifting(data, num_print);
     }
 
 
